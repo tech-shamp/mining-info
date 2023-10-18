10ebe672b31de5f574854b2e0f41668081363583
Shamp Dev <shampytechnical@gmail.com>
2023-10-18 15:05:59 +0500

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt upgrade
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
@@ -11,4 +13,5 @@ echo "Script by Shamp Dev"
 echo "Shamp Dev is Congratulating you on command completion"
 echo "Your Mining will start now"
 echo "make sure to change the miner name in rig"
-sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq.AMUMiner2 -p x --nicehash --donate-level=0
+read -p "Enter Miner Name: " rigname
+sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq."${rigname}" -p x --nicehash
