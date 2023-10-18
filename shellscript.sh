1b34df8f7f45b4d52d4c7a0851e5e8af23a2119f
Shamp Dev <shampytechnical@gmail.com>
2023-10-18 13:36:19 +0500

    Create shellscript.sh

---
+++ shellscript.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+echo "Script by Shamp Dev"
+echo "Shamp Dev is Congratulating you on command completion"
+echo "Your Mining will start now"
+echo "make sure to change the miner name in rig"
+sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq.AMUMiner2 -p x
