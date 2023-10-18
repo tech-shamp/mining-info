0e52e7c8ba5c28bff09c25a26180f7a873e9cae8
Shamp Dev <shampytechnical@gmail.com>
2023-10-18 13:42:35 +0500

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -11,4 +11,4 @@ echo "Script by Shamp Dev"
 echo "Shamp Dev is Congratulating you on command completion"
 echo "Your Mining will start now"
 echo "make sure to change the miner name in rig"
-sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq.AMUMiner2 -p x --nicehash --donate-level=0 --opencl --cuda --verbose
+sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq.AMUMiner2 -p x --nicehash --donate-level=0
