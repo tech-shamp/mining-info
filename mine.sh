f7dd854f6c716f3ed12066290ba27f49c9bfeb53
Shamp Dev <shampytechnical@gmail.com>
2023-10-18 14:18:47 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,12 @@
-sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq.AMUMiner2 -p x --nicehash --donate-level=0
+#!/bin/bash
+TDir="xmrig/build"
+CDir=$(pwd)
+
+if [ "$CDir" != "$TDir" ]; then
+  cd "$TDir"
+  echo "Enter Rig Name: " && read rigname
+  sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq.${rigname} -p x --nicehash --donate-level=0
+else
+  echo "Enter Rig Name: " && read rigname
+  sudo ./xmrig -a rx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbDTWt9VPeq8RJDabJUzY9y1DPnry5dgRGq.${rigname} -p x --nicehash --donate-level=0
+fi
